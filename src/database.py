92a750319f3d71f99b6ceb8078b6d6c8eef71473
Андрей Полушин <pandser@yandex.ru>
2025-09-01 10:03:55 +0300

    create db_helper

---
+++ src/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from src.settings import settings
+
+
+class DatabaseHelper:
+    def __init__(self, url: str):
+        self.engine = create_async_engine(url=url)
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            expire_on_commit=False,
+        )
+
+
+db_helper = DatabaseHelper(
+    url=settings.db_url,
+)
