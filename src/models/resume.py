3b0c405baa8b28fe68b10171f9f5d1b1ec939709
Андрей Полушин <pandser@yandex.ru>
2025-09-01 09:05:27 +0300

    creame resume model

--- src/models/resume.py
+++ src/models/resume.py
@@ -1,3 +1,4 @@
+from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -6,3 +7,9 @@ class Base(DeclarativeBase):
 
     id:  Mapped[int] = mapped_column(primary_key=True)
 
+
+class ResumeModel(Base):
+    __tablename__ = 'resumes'
+
+    title: Mapped[str] = mapped_column(String(30))
+    content: Mapped[str]
